ea0def01a4160973d920b27839b7941912ea7303
Robin Gilks <g8ecj@gilks.org>
2012-10-07 22:53:50 +1300

    Tweak comments

--- median.c
+++ median.c
@@ -1,4 +1,23 @@
-
+//---------------------------------------------------------------------------
+// Copyright (C) 2012 Robin Gilks (but see below for the original authors)
+//
+//
+//  median.c   -   This module provides a set of functions that allow glitches to be detected and ignored.
+//
+//  History:   1.0 - First release. 
+//
+//    This program is free software; you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation; either version 2 of the License.
+//
+//    This program is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with this program; if not, write to the Free Software
+//    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 // 
 //    FILE: RunningMedian.h
 //  AUTHOR: Rob dot Tillaart at gmail dot com  
@@ -116,4 +135,3 @@ bool median_getStatus(MEDIAN *M)
 }
 
 
-// --- END OF FILE ---
